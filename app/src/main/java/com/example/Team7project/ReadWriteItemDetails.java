0a33ce08aaf4e8d08f65570a36592f531058bf1b
Dondapati <s555671@nwmissouri.edu>
2023-04-14 21:30:33 -0500

    Committing changes for milestone 3

---
+++ app/src/main/java/com/example/Team7project/ReadWriteItemDetails.java
@@ -0,0 +1,18 @@
+package com.example.Team7project;
+
+public class ReadWriteItemDetails {
+    public   String name,description,price,Image_url,genere,mobile;
+
+    public ReadWriteItemDetails(String name, String description, String price, String image_url, String genere, String mobile) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.Image_url=image_url;
+        this.genere=genere;
+        this.mobile = mobile;
+
+    }
+    public ReadWriteItemDetails(){
+
+    }
+}
