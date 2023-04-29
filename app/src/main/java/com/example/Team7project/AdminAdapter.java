13a60e5b62aff74784ba858444693dc0140ae303
aparnad7 <112196071+aparnad7@users.noreply.github.com>
2023-04-29 14:26:33 -0500

    Updated a text of a TextView called mItemDate

--- app/src/main/java/com/example/Team7project/AdminAdapter.java
+++ app/src/main/java/com/example/Team7project/AdminAdapter.java
@@ -75,7 +75,8 @@ public class AdminAdapter  extends RecyclerView.Adapter<AdminAdapter.ExampleView
         holder.mItemName.setText(currentItem.getItemName());
         String priceval="Price: "+currentItem.getItemPrice()+"$";
         holder.mItemPrice.setText(priceval);
-        
+        String dateVal="Date: "+currentItem.getItemDate();
+        holder.mItemDate.setText(dateVal);
         Picasso.with(holder.mItemimage.getContext()).load(currentItem.getImageUrl()).into(holder.mItemimage);
        
     }
