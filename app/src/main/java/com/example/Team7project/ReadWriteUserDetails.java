2c3b17327f88d2edc5bffa0a5042ec3ffb2ca014
NikhilDeekshit45 <nikhildeekshit2@gmail.com>
2023-04-29 15:50:18 -0500

    readwriteuserdetails

--- app/src/main/java/com/example/Team7project/ReadWriteUserDetails.java
+++ app/src/main/java/com/example/Team7project/ReadWriteUserDetails.java
@@ -8,7 +8,6 @@ public class ReadWriteUserDetails {
         this.name = name;
         this.age = age;
         this.mobile = mobile;
-        this.gender = gender;
-        this.storeowner=storeowner;
+    
     }
 }
