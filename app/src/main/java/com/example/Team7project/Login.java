f4042df1215c616f2beee3f23ca56a2c907b6811
Anaparthi Sai Ganesh <40638166+anaparthisaiganesh@users.noreply.github.com>
2023-04-29 14:56:05 -0500

    Update Login.java
    Updated the login page

--- app/src/main/java/com/example/Team7project/Login.java
+++ app/src/main/java/com/example/Team7project/Login.java
@@ -123,6 +123,10 @@ public class Login extends AppCompatActivity {
                     }
                 }
             }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
            
         });
     }
